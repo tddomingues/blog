677f07b5f78f00b1b1c8be9e888c400d0433d763
tiago domingues <ttx8000@gmail.com>
2024-09-20 11:13:17 -0300

    chore: configurado o tailwind e o shadcn

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-primary">primary</div>
+      <div className="bg-secondary">secondary</div>
+      <div className="bg-background">background</div>
+      <div className="bg-foreground">foreground</div>
+      <div className="bg-destructive">destructive</div>
+    </div>
+  );
+}
