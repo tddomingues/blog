3e1648ddedfdccc9fd6189b1ea9674305db15019
tiago domingues <ttx8000@gmail.com>
2024-10-06 12:28:06 -0300

    fix: lógica para aparecer o visual da ação de like adicionado

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -6,11 +6,12 @@ import { ThumbsUp } from "lucide-react";
 
 import likePost from "../actions/likePost";
 import { cn } from "../lib/utils";
+import PostProps from "../types/post";
 
 interface HandleLikeProps {
   id_user: string;
   id_post: string;
-  post: any;
+  post: PostProps;
 }
 
 const Like = ({ id_post, id_user, post }: HandleLikeProps) => {
@@ -31,7 +32,7 @@ const Like = ({ id_post, id_user, post }: HandleLikeProps) => {
       >
         <ThumbsUp
           className={cn(
-            post.likes.fk_user_id === id_user
+            post.likes.some((like) => like.fk_user_id === id_user)
               ? "fill-blue-400 text-blue-400"
               : "fill-primary-foreground text-primary"
           )}
